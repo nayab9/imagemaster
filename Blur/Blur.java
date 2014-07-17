6b0a8ba967f7d360c6a1dd2cb5535671076c1367
Ben Houshmand <nayab9@gmail.com>
2014-07-17 01:36:32 -0400

    comments complete for blur explanation

--- Blur/Blur.java
+++ Blur/Blur.java
@@ -3,35 +3,47 @@ package Blur;
 import java.awt.image.BufferedImage;
 import javax.imageio.ImageIO;
 import java.io.*;
-
 import java.awt.image.BufferedImageOp;
 import java.awt.image.ConvolveOp;
 import java.awt.image.Kernel;
 
 public class Blur
 {
-	//TODO: take kernel as an input parameter instead of hardcode
+	String src = null;
+	String src = null;
+	BufferedImage img = null;
+	
+	//TODO: take kernel as an input parameter instead of hardcoded
 	float[] kernel = {
 	1/9f, 1/9f, 1/9f,
 	1/9f, 1/9f, 1/9f,
 	1/9f, 1/9f, 1/9f
 	};
 	
-	String src, dst;
-	
-	BufferedImage img = null;
+	//TODO: determine the size of the kernel instead of hardcoding
 	BufferedImageOp blurKernel = new ConvolveOp(new Kernel(3, 3, kernel));
 	
+	//simple constructor to take the specified source file
+	//and specify the output file
 	public Blur(String src)
 	{
 		this.src = src;
 		this.dst = "blur-" + src;
 	}
 	
-	//demonstrate how the blur actually works, the matrix itself
-	// x x x
-	// x x x
-	// x x x
+	//this call will process the blur itself
+	//a simple blur technique is taking the specified kernel (flipped), aligning its center
+	//to each pixel in our image; then multiplying and adding the matrices.
+	//  i1  i2  i3  i4 i5             x1 x2 x3
+	//  i6  i7  i8  i9 i10  = image   x4 x5 x6  = kernel
+	// i11 i12 i13 i14 i15            x7 x8 x9 
+	// i16 i17 i18 i19 i20
+	//
+	// as an example, to calculate the blur value for i7 above, we do the following:
+	// i1 by x1 + i2 by x2 + i3 by x3 + 
+	// i6 by x4 + i7 by x5 + i8 by x6 +
+	// i11 by x7 + i12 by x8 + i18 by x9
+	
 	public void runBlur()
 	{
 		try
--- Core.java
+++ Core.java
@@ -4,19 +4,25 @@ public class Core
 {
     public static void main(String[] args)
     {
+		//simple parameter parsing, no error checking
 		String file = args[0];
 		String func = args[1];
 		String param = "0";
 		
+		//some techniques require a parameter
 		if (args.length > 2)
 		{
 			param = args[2];
 		}
 		
+		//verbosity to state processing information
 		System.out.println("Requested file: " + file);
 		System.out.println("Image Process type: " + func);
 		System.out.println("Paramter: " + param);
 
+		//a simple technique dispatcher
+		//call the appropriate class to process the image
+		//as requested by the user
 		if (func.equals("-blur"))
 		{
 			System.out.println("Blurring image");
